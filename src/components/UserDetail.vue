02945c6fb4a42f9b9d6ed9e187e974e5686d69a5
Emre Akurek <emreyeprem@gmail.com>
2019-03-20 12:18:08 -0500

    props

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -2,13 +2,23 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
-        <p>User Name: {{ userName }}</p>
+        <p>User Name: {{ switchName() }}</p>
     </div>
 </template>
 
 <script>
   export default {
-      props: ['userName']
+      props: {
+          myName: {
+              type: String,
+              required: true
+          }     
+      },
+      methods: {
+          switchName(){
+              return this.myName.split('').reverse().join('')
+          }
+      }
   }
 </script>
 
