ace6a7e4f80d1117d912f73d3d9fced6c1f74713
Emre Akurek <emreyeprem@gmail.com>
2019-03-21 15:49:52 -0500

    eventBus

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 
 export const eventBus = new Vue({
+  data: {
+
+  },
   methods: {
     changeAge(age) {
-      this.$emit('ageWasEdited', age);
+      this.$emit('ageWasEdited', age);       // you can access this data anywhere in the app as long as you import it in that file.
     }
   }
 });
